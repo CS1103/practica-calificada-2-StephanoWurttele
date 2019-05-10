af103c70e384d3c5bab25a9512434f55d067ad70
StephanoWurttele <39769681+StephanoWurttele@users.noreply.github.com>
2019-05-10 13:30:45 -0500

    Update Fighter.cpp

--- Fighter.cpp
+++ Fighter.cpp
@@ -11,6 +11,8 @@ Fighter::Fighter(string defe[3],string atck[3]){
             def.push_back(*a);
         }
         if(defe[i]=="S"){
+            Shield* b=new Shield();
+            def.push_back(*b);
             def.push_back(*(new Shield()));
         }
         if(defe[i]=="A"){
@@ -23,16 +25,15 @@ Fighter::Fighter(string defe[3],string atck[3]){
     i=0;
     while(atck[i]!=""){
         if(defe[i]=="P"){
-            Punch* d=new Punch();
-            atk.push_back(*d);
+            Punch* a=new Punch();
+            atk.push_back(*a);
         }
         if(defe[i]=="S"){
-            Saber* e=new Saber();
+            Saber* b=new Saber();
+            atk.push_back(*b);
             atk.push_back(*(new Saber()));
         }
         if(defe[i]=="F"){
-            Firearm* f=new Firearm();
-            atk.push_back(*f);
             atk.push_back(*(new Firearm()));
         }
         i++;
@@ -44,23 +45,22 @@ int Fighter::defense() {
     for (int i=0;i<def.size()-1;i++) {
         suma+=def[i].defense();
     }
-    cout<<suma<<endl;
     return suma;
 }
 
 int Fighter::attack() {
     int suma=0;
-    for (int i=0;i<atk.size()-1;i++) {
+    for (int i=0;i<atk.size();i++) {
         suma+=atk[i].attack();
     }
     return suma;
 }
 
-JasonStatham::JasonStatham(string *def3, string *atk3) : Fighter(def3, atk3) {
+JasonStatham::JasonStatham(string *def, string *atk) : Fighter(def, atk) {
 
 }
 
-ChuckNorris::ChuckNorris(string *def2, string *atk2) : Fighter(def2, atk2) {
+ChuckNorris::ChuckNorris(string *def, string *atk) : Fighter(def, atk) {
 
 }
 
